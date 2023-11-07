9f4e26d27bf11d201aec31ea0512af153ea79727
Frida010 <>
2023-11-07 16:42:57 +0000

    Finaly got the categorys to work as I wanted

--- chefblog/urls.py
+++ chefblog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
-from .views import CategoryListView
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('category/<str:category_name>/',
-         CategoryListView.as_view(), name='posts_by_category'),
-    path('category/<str:category_name>/',
-         views.CategoryDetailView.as_view(), name='category_detail'),
+    path('category/<str:category_name>/', views.CategoryListView.as_view(),
+         name='category_posts'),  # Link to category posts
+    path('category/detail/<str:category_name>/', views.CategoryDetailView.as_view(),
+         name='category_detail'),  # Link to category details
+    path('post/<slug:slug>/', views.PostDetail.as_view(),
+         name='post_detail'),  # Link to blogpost details
 ]
--- chefblog/views.py
+++ chefblog/views.py
@@ -1,3 +1,6 @@
+from django.views import View
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
@@ -39,6 +42,7 @@ class CategoryDetailView(ListView):
         context['selected_category'] = category_name
         return context
 
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -47,34 +51,32 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
+    template_name = "post_detail.html" 
 
     def get(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(Post, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             liked = True
 
         return render(
             request,
-            "post_detail.html",
+            self.template_name,
             {
                 "post": post,
                 "comments": comments,
                 "commented": False,
                 "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm() 
             },
         )
 
     def post(self, request, slug, *args, **kwargs):
-
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(Post, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             liked = True
 
         comment_form = CommentForm(data=request.POST)
@@ -89,7 +91,7 @@ class PostDetail(View):
 
         return render(
             request,
-            "post_detail.html",
+            self.template_name,
             {
                 "post": post,
                 "comments": comments,
@@ -98,7 +100,6 @@ class PostDetail(View):
                 "liked": liked
             },
         )
-
 
 class PostLike(View):
 
