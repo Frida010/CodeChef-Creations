56673a33eb285f55b819982eabeeb611eb450ea1
Frida010 <>
2023-11-21 16:48:48 +0000

    Added more testcases for the python code

--- chefblog/tests.py
+++ chefblog/tests.py
@@ -62,3 +62,67 @@ class SavedPostModelTest(TestCase):
         post = Post(title='Test Post', slug='test-post',
                     author=user, content='Test content')
         post.save()
+
+
+# Comment model tests
+class CommentModelTest(TestCase):
+    def test_comment_is_linked_to_correct_post(self):
+        # Creating a test Post instance
+        post = Post.objects.create(
+            title='Test Post', slug='test-post', author=User.objects.create_user(
+                username='testuser', password='testpassword'), content='Test content')
+        # Creating a test Comment instance linked to the test Post
+        comment = Comment(post=post, name='Test Commenter',
+                          email='test@example.com', body='Test comment body')
+        comment.save()
+        # Asserting that the comment is correctly linked to the post
+        self.assertEqual(comment.post, post)
+
+    def test_comment_approval(self):
+        # Creating a test Post instance
+        post = Post.objects.create(
+            title='Test Post', slug='test-post', author=User.objects.create_user(
+                username='testuser', password='testpassword'), content='Test content')
+        # Creating a test Comment instance linked to the test Post
+        comment = Comment(post=post, name='Test Commenter',
+                          email='test@example.com', body='Test comment body')
+        comment.save()
+        # Asserting that the comment starts as not approved
+        self.assertFalse(comment.approved)
+        # Approving the comment and asserting that it is now approved
+        comment.approved = True
+        comment.save()
+        self.assertTrue(comment.approved)
+
+
+# SavedPost model tests
+class SavedPostModelTest(TestCase):
+    def test_saved_post_is_linked_to_correct_user_and_post(self):
+        # Creating a test user
+        user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        # Creating a test Post instance
+        post = Post.objects.create(
+            title='Test Post', slug='test-post', author=user, content='Test content')
+        # Creating a test SavedPost instance linked to the test user and post
+        saved_post = SavedPost(user=user, post=post)
+        saved_post.save()
+        # Asserting that the saved post is correctly linked to the user and post
+        self.assertEqual(saved_post.user, user)
+        self.assertEqual(saved_post.post, post)
+
+    def test_user_can_unsave_post(self):
+        # Creating a test user
+        user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        # Creating a test Post instance
+        post = Post.objects.create(
+            title='Test Post', slug='test-post', author=user, content='Test content')
+        # Creating a test SavedPost instance linked to the test user and post
+        saved_post = SavedPost(user=user, post=post)
+        saved_post.save()
+        # Asserting that the user has a saved post
+        self.assertTrue(user.savedpost_set.exists())
+        # Deleting the saved post and asserting that the user no longer has any saved posts
+        saved_post.delete()
+        self.assertFalse(user.savedpost_set.exists())
